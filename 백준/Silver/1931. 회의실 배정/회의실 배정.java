c160342905e405d14c6cf257f12eb06433425578
RohDamin <57509627+RohDamin@users.noreply.github.com>
2024-02-18 00:29:49 +0900

    [Silver I] Title: 회의실 배정, Time: 508 ms, Memory: 43580 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,50 @@
+// Greedy Algorithm
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int arr[][] = new int [n][2];
+
+
+        for (int i = 0; i<n; i++) {
+            String s = br.readLine();
+            StringTokenizer st = new StringTokenizer(s, " ");
+
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        // 정렬
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0]-o2[0];
+                } else {
+                    return o1[1]-o2[1];
+                }
+            }
+        });
+
+        int count = 1;
+        int target = 0;
+        for (int i = 0; i<n-1; i++) {
+            if (arr[target][1]<arr[i+1][0] || arr[target][1]==arr[i+1][0]) {
+                count++;
+                target = i+1;
+            }
+        }
+
+        System.out.println(count);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
+
