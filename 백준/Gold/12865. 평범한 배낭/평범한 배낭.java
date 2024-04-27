93ed7dffe5e7afaaac5dce150e07da8afee0e800
RohDamin <57509627+RohDamin@users.noreply.github.com>
2024-04-27 20:24:06 +0900

    [Gold V] Title: 평범한 배낭, Time: 196 ms, Memory: 53736 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -6,48 +6,42 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static Integer[][] dp;
-	static int[][] bag;
+	static int[][] dp;
+	static int[] bagW; // 무게
+	static int[] bagV; // 가치
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken()); // 물품의 수
-		int K = Integer.parseInt(st.nextToken()); // 버틸 수 있는 무게
+		int K = Integer.parseInt(st.nextToken()); // 넣을 수 있는 무게
 		
-		dp = new Integer[N][K+1];
-		bag = new int[N][2];
+		dp = new int[N+1][K+1];
+		bagW = new int[N+1];
+		bagV = new int[N+1];
 		
-		for (int i=0; i<N; i++) {
+		for (int i=1; i<=N; i++) {
 			st = new StringTokenizer(br.readLine());
-			int W = Integer.parseInt(st.nextToken()); // 물건의 무게
-			int V = Integer.parseInt(st.nextToken()); // 가치
-			
-			bag[i][0] = W;
-			bag[i][1] = V;
+			bagW[i] = Integer.parseInt(st.nextToken());
+			bagV[i] = Integer.parseInt(st.nextToken());
 		}
-		System.out.print(knapsack(N-1, K));
-	}
-	
-	// top-down 방식
-	static int knapsack(int i, int k) {
-		// i가 범위 밖이라면 0 리턴
-		if (i<0) return 0;
-		
-		// 탐색하지 않은 위치라면
-		if (dp[i][k] == null) { // Integer 배열 -> null로 초기화됨 -> 탐색안한 상태 null
-			
-			// 현재 물건을 담지 못하는 경우
-			if (bag[i][0] > k) dp[i][k] = knapsack(i-1, k); // 이전 가치를 넣어줌
-			
-			// 현재 물건을 담을 수 있는 경우
-			else {
-				// i번째 물건을 선택하면서, i-1번째 물건을 선택하는 경우 중,
-				// [현재 가능한 최대 무게 - i번째 물건의 무게]까지의 최대 가치를 더한 값
-				dp[i][k] = Math.max(knapsack(i-1, k), knapsack(i-1, k-bag[i][0])+bag[i][1]);
+
+		// bottom up 방식 풀이
+		for (int i=1; i<=N; i++) {
+			for (int j=1; j<=K; j++) {
+				
+				// i번째 무게를 더 담을 수 없는 경우
+				if (bagW[i] > j) dp[i][j] = dp[i-1][j];
+				// i번째 무게를 더 담을 수 있는 경우
+				else {
+					// 이전 가치(현재 물건 안담음)의 값과
+					// 현재 물건을 담고(담으면 남은 무게는 j-bagW[i]) 남은 무게의 최대 가치 + 현재 가치(현재 물건을 담았으니까)의 값
+					// 2개를 비교해 더 큰 것을 dp 배열에 저장
+					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-bagW[i]]+bagV[i]);
+				}
 			}
 		}
-		return dp[i][k];
+		System.out.println(dp[N][K]);
 	}
 }
