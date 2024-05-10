7088ca1301b2bb77f9f782a37370f654d4d10a13
RohDamin <57509627+RohDamin@users.noreply.github.com>
2024-05-10 13:17:31 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,38 +1,33 @@
-// Greedy Algorithm
-import java.io.*;
+// Greedy
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        String s = br.readLine();
-        StringTokenizer st = new StringTokenizer(s, " ");
-
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int arr [] = new int [n];
-
-        for (int i = 0; i<n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-
-        int count = 0;
-        while (k>0) {
-            for (int i = n-1; i>=0; i--){
-                if (k>=arr[i]) { // k에서 뺄 수 있는 금액이라면
-                    count += k/arr[i];
-                    k -= (k/arr[i])*arr[i];
-                }
-            }
-        }
-
-        System.out.println(count);
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+	public static void main(String args[]) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] coin = new int[N];
+		int cnt = 0;
+		
+		for (int i=0; i<N; i++) {
+			coin[i] = Integer.parseInt(br.readLine()); 
+		}
+		
+		
+		while (true) {
+			if (K == 0) break;
+			
+			for (int i=N-1; i>=0; i--) {
+				if (K >= coin[i]) {
+					cnt += K/coin[i];
+					K -= (K/coin[i])*coin[i];
+				}
+			}
+		}
+		
+		System.out.println(cnt);
+	}
 }
-
