8005d9af30234dfc489416ae808054fefdc1dd42
RohDamin <rohdamin3@gmail.com>
2024-06-29 22:10:15 +0900

    [Silver III] Title: 소수 구하기, Time: 5180 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,15 @@
+# 수학
+import sys
+import math
+M, N = map(int, sys.stdin.readline().split())
+
+for i in range(M, N+1):
+    if i == 1:
+        continue
+    primeflag = True
+    for j in range(2, int(math.sqrt(i)) + 1):
+        if i % j == 0:
+            primeflag = False
+            break
+    if primeflag:
+        print(i)
