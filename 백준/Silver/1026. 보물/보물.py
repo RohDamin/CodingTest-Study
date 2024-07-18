a24800607e222a76f669b4989b2de46e630fa21d
RohDamin <rohdamin3@gmail.com>
2024-07-18 17:17:53 +0900

    [Silver IV] Title: 보물, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,10 @@
+import sys
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+B = list(map(int, sys.stdin.readline().split()))
+A.sort()
+B.sort(reverse=True)
+sum = 0
+for i in range (len(A)):
+    sum += A[i]*B[i]
+print(sum)
